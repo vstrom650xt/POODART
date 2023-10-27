05d605ea4e82412f5e35bd23183b8d5b5337819e
vstrom60xt <cerveramonzo@gmail.com>
2023-10-27 19:21:05 +0200

    asdsad

--- bin/promesas/promesa.dart
+++ bin/promesas/promesa.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'dart:math';
 
-void main(List<String> args) {
+void main(List<String> args) async {
   print("write a city");
   String city = stdin.readLineSync() ?? '';
   print("object");
